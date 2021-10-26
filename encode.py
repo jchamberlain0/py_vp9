a42606667c47b57818b6dcd13ae9b02ab76769ca
john <54239598+jchamberlain0@users.noreply.github.com>
2021-10-26 16:58:49 -0400

    Single-encode and batch mode both working

---
+++ encode.py
@@ -0,0 +1,47 @@
+import json
+import pprint
+import vp9
+
+# Python 	JSON
+
+# dict 	Object
+# list 	Array
+# tuple 	Array
+# str 	String
+# int 	Number
+# float 	Number
+# True 	true
+# False 	false
+# None 	null
+
+with open('settings.json') as settings_file:
+  settings = json.load(settings_file)
+
+
+if settings['Debug']:
+  pp = pprint.PrettyPrinter(indent=2)
+  pp.pprint(settings)
+
+
+result = False
+filesEncoded = 0
+
+if settings['Batch']:
+  # Batch mode loops over CRF values in the settings and encodes one video for each quality level
+  for crf in settings['CRF']:
+    result = vp9.encodeVP9(crf,settings)
+    filesEncoded = filesEncoded+1
+    if result != True:
+      print("\nAn issue was detected while encoding file with crf "+crf+". Stopping batch mode.")
+      break
+  result = True
+else:
+  result = vp9.encodeVP9("defaultCRF",settings)
+  filesEncoded = filesEncoded+1
+
+
+if result == True:
+  print('\n\nSuccessfully encoded '+str(filesEncoded)+' file(s).'+' Exiting...')
+else:
+  print('\n\nThere was an issue with encoding. Exiting...')
+
---
+++ vp9.py
@@ -0,0 +1,94 @@
+import subprocess
+
+# Encode a single video to VP9 with a fixed constant rate factor.
+# This function is hard coded to use two-pass because there's no use case not to.
+# For simplicity's sake, the second pass is added in sequence,
+#   rather than trying to modify the first pass arg list or something fancy.
+def encodeVP9(crf, settings):
+
+  # List of arguments to pass to ffmpeg
+  firstPass = ['ffmpeg']
+  secondPass = ['ffmpeg']
+
+  # Used to denote scaled resolution in output file
+  horizontalLines = "noscale"
+
+  # Check if a CRF value was passed in - this will be used by batch mode.
+  if crf == "defaultCRF":
+    # Single encode mode uses the CRF from settings.
+    # If a value was passed in, that will be used instead.
+    crf = settings['CRFDefault']
+
+
+
+  firstPass.append("-y")
+  firstPass.append("-i")
+  firstPass.append(settings['FileDir']+settings['InputFilename']+settings['OutputExtension'])
+
+  if settings['Scale']:
+    # These args will be omitted entirely if Scale is false.
+    # TODO: make the horizontallines logic more robust, it will fail on 4-digit resolutions.
+    horizontalLines = settings['OutResolution'][4:7]
+    firstPass.append("-vf")
+    firstPass.append("scale="+settings['OutResolution'])
+    firstPass.append("-sws_flags")
+    firstPass.append(settings['ScaleMode'])
+  
+  firstPass.append("-c:v")
+  firstPass.append("libvpx-vp9")
+  firstPass.append("-b:v")
+  firstPass.append("0")
+  firstPass.append("-crf")
+  firstPass.append(crf)
+  firstPass.append("-pass")
+  firstPass.append("1")
+  firstPass.append("-f")
+  firstPass.append("webm")
+
+  # TODO: add operating system check so this works on Linux.
+  firstPass.append("NUL")
+
+  # End First Pass
+
+  secondPass.append("-i")
+  secondPass.append(settings['FileDir']+settings['InputFilename']+settings['OutputExtension'])
+
+  if settings['Scale']:
+    secondPass.append("-vf")
+    secondPass.append("scale="+settings['OutResolution'])
+    secondPass.append("-sws_flags")
+    secondPass.append(settings['ScaleMode'])
+  
+  secondPass.append("-c:v")
+  secondPass.append("libvpx-vp9")
+  secondPass.append("-b:v")
+  secondPass.append("0")
+  secondPass.append("-crf")
+
+  # Check if a CRF value was passed in - this will be used by batch mode.
+  if crf == "defaultCRF":
+    # Single encode mode uses the CRF from settings.
+    secondPass.append(settings['CRFDefault'])
+  else:
+    # Batch mode passes CRF in.
+    secondPass.append(crf)
+  
+  secondPass.append("-pass")
+  secondPass.append("2")
+  secondPass.append("-c:a")
+  secondPass.append("libopus")
+  secondPass.append(settings['OutFileDir']+settings['InputFilename']+"_crf"+crf+"_"+horizontalLines+settings['OutputExtension'])
+  
+  
+
+  print("Running first pass.")
+  print(firstPass)
+  # commandResult = subprocess.run(ff_args, capture_output=True)
+
+  print("Running second pass.")
+  print(secondPass)
+
+  return False
+
+  # commandResult = subprocess.run(['ls','-l'], capture_output=True)
+  # print(commandResult)
